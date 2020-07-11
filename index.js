8eef4fa30a48098af442a27043ce060f55274c9c
Marek Fořt <marek.fort@ackee.cz>
2020-07-11 13:39:16 +0200

    Add access token.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ try {
     execSync('mint install fortmarek/tapestry@feature/github_release --force');
     const tag = execSync('git describe --tags').toString();
     execSync(`git tag -d ${tag}`);
-    execSync(`tapestry release ${tag}`);
+    execSync(`tapestry action docs-update ${tag}`);
+    execSync(`TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_PERSONAL_ACCESS_TOKEN} tapestry release ${tag}`);
   } catch (error) {
     core.setFailed(error.message);
   }
