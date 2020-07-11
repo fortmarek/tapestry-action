6a51bb3fe3c85696c13b638af13a38fc599c0115
Marek Fořt <marek.fort@ackee.cz>
2020-07-11 16:52:54 +0200

    Add github actor to access token.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { execSync, exec } = require("child_process");
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
     execSync('brew install mint');
@@ -8,7 +9,7 @@ try {
     execSync(`git tag -d ${tag}`);
     execSync(`tapestry action docs-update ${tag}`);
     execSync(
-        `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_PERSONAL_ACCESS_TOKEN} tapestry release ${tag}`,
+        `TAPESTRY_ACCESS_TOKEN=${github.actor}:${process.env.GITHUB_PERSONAL_ACCESS_TOKEN} tapestry release ${tag}`,
         {stdio: 'inherit'}
     );
   } catch (error) {
