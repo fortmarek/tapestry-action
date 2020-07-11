f91253f36d6a9cf27cba9d16c2744c8507bb114a
Marek Fořt <marek.fort@ackee.cz>
2020-07-11 13:14:54 +0200

    Read tag.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const { execSync } = require("child_process");
+const { execSync, exec } = require("child_process");
+const github = require('@actions/github');
+const core = require('@actions/core');
 
 try {
-    execSync()
+    execSync('brew install mint');
+    execSync('mint install fortmarek/tapestry@master --force');
+    const tag = execSync('git describe --tags').toString();
+    execSync(`git tag -d ${tag}`);
+    execSync(`tapestry release ${tag}`);
   } catch (error) {
     core.setFailed(error.message);
   }
