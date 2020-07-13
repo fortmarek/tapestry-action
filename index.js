e3fde64e6abad2640280e4fb544a88ecf83d054b
Marek Fořt <marek.fort@ackee.cz>
2020-07-13 21:10:36 +0200

    Remove git setup.

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ try {
     execSync('mint install fortmarek/tapestry@feature/github_release --force');
     const tag = execSync('git describe --tags').toString();
     execSync(`git tag -d ${tag}`);
-    execSync('git config --global user.email "fortmarek@users.noreply.github.com"');
-    execSync('git config --global user.name "fortmarek"');
     execSync(
         `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
         {stdio: 'inherit'}
