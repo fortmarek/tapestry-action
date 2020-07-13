d85e09aa5f9487783ec3bed405f83396a2fa760a
Marek Fořt <marek.fort@ackee.cz>
2020-07-13 21:03:36 +0200

    Remove test calls.

--- index.js
+++ index.js
@@ -9,14 +9,10 @@ try {
     execSync(`git tag -d ${tag}`);
     execSync('git config --global user.email "fortmarek@users.noreply.github.com"');
     execSync('git config --global user.name "fortmarek"');
-    execSync('touch hello');
-    execSync('git add hello');
-    execSync('git commit -am "commit"');
-    execSync('git push');
-    // execSync(
-    //     `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
-    //     {stdio: 'inherit'}
-    // );
+    execSync(
+        `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
+        {stdio: 'inherit'}
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
