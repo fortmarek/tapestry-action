76fe10fe57017b4944a8007ae02e138d93427855
Marek Fořt <marek.fort@ackee.cz>
2020-07-15 08:13:32 +0200

    Remove tapestry action.

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    execSync('brew install mint');
-    execSync('mint install fortmarek/tapestry@master --force');
+    // execSync('brew install mint');
+    // execSync('mint install fortmarek/tapestry@master --force');
     const tag = execSync('git describe --tags --abbrev=0').toString();
     execSync(`git push --delete origin ${tag}`)
     execSync(`git tag -d ${tag}`);
-    execSync(
-        `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
-        {stdio: 'inherit'}
-    );
+    execSync(`git tag ${tag}`);
+    execSync(`git push --tags`);
+    // execSync(
+    //     `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
+    //     {stdio: 'inherit'}
+    // );
   } catch (error) {
     core.setFailed(error.message);
   }
