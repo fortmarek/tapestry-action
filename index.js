9727991ae28783054dabb7ce1e9598e6ef1deb92
Marek Fořt <marek.fort@ackee.cz>
2020-07-16 21:00:00 +0200

    Add git account.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const github = require('@actions/github');
 try {
     // execSync('brew install mint');
     // execSync('mint install fortmarek/tapestry@master --force');
+    execSync('git config --local user.email "action@github.com"');
+    execSync('git config --local user.name "GitHub Action"');
     const tag = execSync('git describe --tags --abbrev=0').toString();
     execSync(`git push --delete origin ${tag}`)
     execSync(`git tag -d ${tag}`);
