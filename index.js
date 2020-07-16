d026d3a0392e30ae459768e933b053afef59e86b
Marek Fořt <marek.fort@ackee.cz>
2020-07-16 21:16:23 +0200

    Run tapestry.

--- index.js
+++ index.js
@@ -3,23 +3,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // execSync('brew install mint');
-    // execSync('mint install fortmarek/tapestry@master --force');
+    execSync('brew install mint');
+    execSync('mint install fortmarek/tapestry@master --force');
     execSync('git config --local user.email "action@github.com"');
     execSync('git config --local user.name "GitHub Action"');
     const tag = execSync('git describe --tags --abbrev=0').toString();
     execSync(`git push --delete origin ${tag}`)
     execSync(`git tag -d ${tag}`);
-    execSync(`git tag ${tag}`);
-    execSync(`git push --tags`);
-    execSync('touch hello');
-    execSync('git add hello');
-    execSync('git commit -m "Try commit"');
-    execSync('git push');
-    // execSync(
-    //     `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
-    //     {stdio: 'inherit'}
-    // );
+    execSync(
+        `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
+        {stdio: 'inherit'}
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
