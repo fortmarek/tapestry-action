91fc66808ec8a64f559ce4b9ee6c40a5f9daa2e0
Marek Fořt <marek.fort@ackee.cz>
2020-07-23 18:56:20 +0200

    Remove unused call.

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // execSync(`curl --request POST \
-  //       --url https://api.github.com/repos/tapestry/issues \
-  //       --header 'authorization: Bearer ${process.env.GITHUB_TOKEN}' \
-  //       --header 'content-type: application/json' \
-  //       --data '{
-  //         "title": "Test issue",
-  //         "body": "This issue was automatically created by the GitHub Action workflow."
-  //         }\'`);
     execSync('brew install mint');
     execSync('mint install fortmarek/tapestry@master --force');
     execSync('git config --local user.email "action@github.com"');
