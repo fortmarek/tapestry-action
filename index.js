4e0a218b15cd42449b42bec43d5da526ae4a5cc2
Marek Fořt <marek.fort@ackee.cz>
2020-12-23 18:57:17 +0100

    Add RELEASE_ACTOR option.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const { execSync, exec } = require("child_process");
 const core = require('@actions/core');
+const { release } = require("os");
 
 try {
     execSync('brew install mint');
     execSync('mint install AckeeCZ/tapestry');
-    execSync('git config --local user.email "action@github.com"');
-    execSync('git config --local user.name "GitHub Action"');
+    if (!process.env.RELEASE_ACTOR) {
+      execSync('git config --local user.email "action@github.com"');
+      execSync('git config --local user.name "GitHub Action"');
+    }
     const tag = execSync('git describe --tags --abbrev=0').toString();
     execSync(`git push --delete origin ${tag}`)
     execSync(`git tag -d ${tag}`);
+    const releaseActor = process.env.RELEASE_ACTOR ? process.env.RELEASE_ACTOR : process.env.GITHUB_ACTOR;
     execSync(
-        `TAPESTRY_ACCESS_TOKEN=${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
+        `TAPESTRY_ACCESS_TOKEN=${releaseActor}:${process.env.GITHUB_TOKEN} tapestry release ${tag}`,
         {stdio: 'inherit'}
     );
   } catch (error) {
